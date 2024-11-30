9c376c2d1202e6a1907ceabf39abc87acdc4ad06
edoyang <edoyangz@gmail.com>
2024-11-30 23:27:10 +1100

    fixing the item soon

--- client/src/pages/GetProducts/index.jsx
+++ client/src/pages/GetProducts/index.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
+import "./style.scss";
 
 const GetProducts = () => {
   const [products, setProducts] = useState([]); // State to store fetched products
