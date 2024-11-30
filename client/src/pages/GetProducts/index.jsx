6a2fc9e682059022cfae3499bfdbf67a28087d1f
edoyang <edoyangz@gmail.com>
2024-11-30 22:14:19 +1100

    uninstall quagga

---
+++ client/src/pages/GetProducts/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GetProducts = () => {
+  return (
+    <div className="get-products">
+      <h1>Get Products</h1>
+    </div>
+  );
+};
+
+export default GetProducts;
